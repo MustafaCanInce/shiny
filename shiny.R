48f45571a467925695a41ea171f78e0c1b7e6491
Gizem Ozgen <88457431+GizemOzgen@users.noreply.github.com>
2022-12-06 18:20:42 +0300

    Update shiny.R

--- shiny.R
+++ shiny.R
@@ -1,13 +1,15 @@
 library(shiny);
 library(tidyverse)
 library(fontawesome)
+library(jpeg)
 
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(width=1080,
                  #actionButton("plotpoints", label = "Show Dots"),
                  #actionButton("stopaddpoint", label = "Stop"),
-                 verbatimTextOutput("info")
+                 verbatimTextOutput("info"),
+                 actionButton("clear", label = "Clear Points")
     ),
     
     mainPanel(
@@ -20,16 +22,16 @@ ui <- fluidPage(
     )
   ),
   # Upload Image Button
-  absolutePanel(wellPanel(actionButton(icon=NULL, inputId="upload_Button ", width=100, label="Upload Photo")),   top=300, height=200, left='160vh', width=150),
+  absolutePanel(wellPanel(actionButton(icon=NULL, inputId="upload_Button ", width=100, label="Upload Photo")),   top=300, height=200, left='170vh', width=150),
   
   # Next Image Button
-  absolutePanel(wellPanel(actionButton(icon=NULL, inputId="next_Button"   , width=100, label="Next Image")),     top=500, height=200, left='160vh', width=150),
+  absolutePanel(wellPanel(actionButton(icon=NULL, inputId="next_Button"   , width=100, label="Next Image")),     top=450, height=200, left='170vh', width=150),
   
   # Previous Image Button
-  absolutePanel(wellPanel(actionButton(icon=NULL, inputId="prev_Button"   , width=100, label="Previous Image")), top=700, height=200, left='160vh', width=150),
+  absolutePanel(wellPanel(actionButton(icon=NULL, inputId="prev_Button"   , width=100, label="Previous Image")), top=600, height=200, left='170vh', width=150),
   
   # Missing Point Button
-  absolutePanel(wellPanel(actionButton(icon=NULL, inputId="missing_Button", width=100, label="Missing point")),  top=900, height=200, left='160vh', width=150),
+  absolutePanel(wellPanel(actionButton(icon=NULL, inputId="missing_Button", width=100, label="Missing point")),  top=750, height=200, left='170vh', width=150),
   
   #Image Table Output
   #absolutePanel(wellPanel(dataTableOutput(outputId="image_table_Output")), top=150, height=300, left=25, width=975),
@@ -54,18 +56,19 @@ server <- function(input, output, session) {
   
   xy_new <- reactiveValues(x= numeric(0), y = numeric(0), line=numeric(0)) # add new points
   
-
+  
   
   
   output$plot.ui <- renderUI({
     plotOutput("distplot",
                click = "plot_click",
-               dblclick = "plot_dblclick",#dblclick is not shown as a point in plot
+               dblclick = "plot_dblclick",
+               #dblclick is not shown as a point in plot
                #hover = "plot_hover",
-               brush = "plot_brush",
+               #brush = "plot_brush",
                height = "80vh",
                width = "160vh"
-               )
+    )
   })
   
   # Listen for clicks and store values
@@ -85,21 +88,59 @@ server <- function(input, output, session) {
     cat(input$GetScreenWidth)
   })
   
+  #pointsforplot <- eventReactive(input$clear, ignoreNULL = F,{
+  #  tibble(x = NULL, y = NULL)
+  #})
   
   # Get the click values on button click
   #pointsforplot <- eventReactive(input$plotpoints, ignoreNULL = F, {
-    
-   # tibble(x = xy_new$x, y = xy_new$y)
-    
+  
+  # tibble(x = xy_new$x, y = xy_new$y)
+  
   #})
+  
+  
+  #eventReactive for upload photo action button
+  #upload_image <- eventReactive(input$upload_Button,{
+    
+    #to load the image
+    #ima <- readJPEG("C:/coordinates/bone.jpg")
+    
+    #take plot info and set background photo
+    #lim <- par()
+    #rasterImage(ima, lim$usr[1], lim$usr[3], lim$usr[2], lim$usr[4])
+  #})
+  
   
   output$distplot <- renderPlot({
     
     # Will update on button click, refreshing the plot
     coord <- tibble(x = xy_new$x, y = xy_new$y)
-      #pointsforplot()
+    #to load the image
+    ima <- readJPEG("C:/coordinates/bone.jpg")
     
-    plot(coord$x, coord$y, xlim=c(-400, 400), ylim=c(-500, 500), xlab="X", ylab="Y")
+    #to save the coordinates of the dots
+    fileName <- file("C:/coordinates/coord.txt")
+    imageName <- basename("C:/coordinates/bone.jpg")
+    
+    coordinates <- c(as.character(xy_new$x), "\n", as.character(xy_new$y))
+    writeLines(c(imageName, length(xy_new$x), coordinates),fileName)
+    close(fileName)
+    
+    #to load the image
+    #ima <- readJPEG("C:/coordinates/bone.jpg")
+    
+    #pointsforplot()
+    plot(coord$x, coord$y, xlim=c(-600, 600), ylim=c(-600, 600), xlab="X", ylab="Y")
+    
+    
+    #uploads <- upload_image()
+    
+    #take plot info and set background photo
+    lim <- par()
+    rasterImage(ima, lim$usr[1], lim$usr[3], lim$usr[2], lim$usr[4])
+    
+    
     
   })
   
@@ -119,9 +160,9 @@ server <- function(input, output, session) {
     
     paste0(
       "click: ", xy_str(input$plot_click),
-      "double_click: ", xy_str(input$plot_dblclick),
+      "double_click: ", xy_str(input$plot_dblclick)
       #"hover: ", xy_str(input$plot_hover),
-      "brush: ", xy_range_str(input$plot_brush)
+      #"brush: ", xy_range_str(input$plot_brush)
     )
   })
 }
